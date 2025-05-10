224ef59b53b636ef349761d068d28223892bf160
Gringolucas <lucastrucco83@gmail.com>
2025-05-10 09:04:06 -0300

    primer bosquejo

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  goToTexts() {
+    this.router.navigate(['/textos']);
+  }
+}
+
